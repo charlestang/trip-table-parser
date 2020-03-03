42d59541b3eda68a48ae6daf83a7122f7cc406ba
Charles <charlestang@foxmail.com>
2020-03-03 17:11:48 +0800

    增加了错误处理

--- trip_table_parser.py
+++ trip_table_parser.py
@@ -1,5 +1,9 @@
-"""这是一个小工具，用于将滴滴出行、高德地图等打车记录
-形成的 PDF 格式电子行程单，转换成文本，CSV 或者 Excel 等格式。"""
+"""网约车平台 PDF 格式电子行程单解析器
+
+滴滴出行，高德地图等当代打车软件，都会提供电子行程单导出功能，主要用于财务报销时候的凭据。
+但是 PDF 格式不方便系统进行数据的分析和读取，本工具提供了一个简单的命令，用于将 PDF 格式
+行程单导出成 CSV 格式或者 Excel 格式。
+"""
 import os
 import sys
 import argparse
@@ -12,12 +16,7 @@ from codecs import BOM_UTF8
 from argparse import ArgumentParser
 from argparse import ArgumentDefaultsHelpFormatter
 from pdfminer.high_level import extract_text
-
-
-def _read_as_text(file_path):
-    trip_table_file_content = extract_text(file_path)
-    whole_file = [x for x in filter(lambda x: x.strip() != '', "".join(trip_table_file_content).splitlines())]
-    return ''.join(whole_file)
+from pdfminer.pdfparser import PDFSyntaxError
 
 
 def _parse_didi(file_path, line_count=0):
@@ -37,7 +36,7 @@ def _parse_didi(file_path, line_count=0):
 
 
 def _parse_gaode(file_path, line_count=0):
-    """解析高德地图的行程单，表头行需要特别清洗"""
+    """解析高德地图的行程单"""
     gaode_area = [173, 37.5767, 791.3437, 559.1864]
     if line_count != 0:
         gaode_area[2] = 216.9033 + line_count * 30
@@ -87,7 +86,7 @@ def _parse_meituan(file_path, line_count=0):
         meituan_area[2] = 314.7975 + 28.305 * line_count
     dfs = tabula.read_pdf(file_path, pages='1', area=meituan_area, stream=True)
     if 0 == len(dfs):
-        logging.errors("no table found")
+        logging.error("no table found")
     elif 1 < len(dfs):
         logging.warning("more than 1 table recognized")
 
@@ -128,16 +127,36 @@ def _output_excel(df, output_path):
     """利用 DataFrame 自身的 API，导出到 Excel 格式"""
     df.to_excel(output_path, index=False, sheet_name='Sheet1')
 
+def _output(df, file_type):
+    extension = {'csv': 'csv', 'excel': 'xlsx'}
+    if file_type in ['csv', 'excel']:
+        exporter = getattr(sys.modules[__name__], '_output_' + file_type)
+        exporter(df, 'output.' + extension[file_type])
+    else:
+        logging.error('不支持的导出文件类型，目前仅支持 CSV 和 Excel')
+
+
 platform_pattern = {
-    'didi':    {'title_like': r'滴滴出行', 'line_count_like': r'共(\d+)笔行程', 'parser': _parse_didi},
-    'gaode':   {'title_like': r'高德地图', 'line_count_like': r'共计(\d+)单行程', 'parser': _parse_gaode},
-    'shouqi':  {'title_like': r'首汽约车电子行程单', 'line_count_like': r'共(\d+)个行程', 'parser': _parse_shouqi},
-    'meituan': {'title_like': r'美团打车', 'line_count_like': r'(\d+)笔行程', 'parser': _parse_meituan}
+    'didi':    {'title_like': '滴滴出行', 'line_count_like': r'共(\d+)笔行程', 'parser': _parse_didi},
+    'gaode':   {'title_like': '高德地图', 'line_count_like': r'共计(\d+)单行程', 'parser': _parse_gaode},
+    'shouqi':  {'title_like': '首汽约车电子行程单', 'line_count_like': r'共(\d+)个行程', 'parser': _parse_shouqi},
+    'meituan': {'title_like': '美团打车', 'line_count_like': r'(\d+)笔行程', 'parser': _parse_meituan}
 }
 
 
-def _read_meta(file_content):
+def _extract_text(file_path):
+    try:
+        pdf_to_text = extract_text(file_path)
+        return ''.join([x for x in filter(lambda x: x.strip() != '', "".join(pdf_to_text).splitlines())])
+    except PDFSyntaxError as pse:
+        logging.error('文件解析错误，不是一个正确的 PDF 文件')
+        raise Exception('无法解析 PDF') from pse
+    return ''
+
+
+def _read_meta(file_path):
     """读取行程单的信息，识别平台、行数、页数等"""
+    file_content = _extract_text(file_path)
     platform = 'unknown'
     line_count = 0
     parser = _parse_unknown
@@ -157,30 +176,15 @@ def main(args=None):
                             formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('file_path', metavar='<FILE>', type=str,
                         help='需要处理的行程单文件')
-    parser.add_argument('--platform', '-p', metavar='<P>', type=str, default='unknown',
-                        help='来源的商户: didi，gaode，shouqi，meituan，unknown')
-    parser.add_argument('--output', '-o', metavar='<FILE NAME>', type=str, default='output.csv',
-                        help='输出的文件名，默认是 output.csv，如果类型是 excel，则是 output.xlsx')
     parser.add_argument('--output_type', '-t', metavar='<TYPE>', type=str, default='csv',
                         help='输出文件类型，默认是csv，也可以是excel')
     args = parser.parse_args(args)
 
-    read_as_string = _read_as_text(args.file_path)
-
-    args.platform, line_count, parser = _read_meta(read_as_string)
-    print("识别出来的平台是：%s，行程行数是：%d" % (args.platform, line_count))
+    platform, line_count, parser = _read_meta(args.file_path)
+    print("识别出来的平台是：%s，行程行数是：%d" % (platform, line_count))
 
     df = parser(args.file_path, line_count)
-
-    if args.output_type == 'csv':
-        _output_csv(df, args.output)
-    elif args.output_type == 'excel':
-        # 如果使用默认参数的话，改一下后缀
-        if args.output == 'output.csv':
-            args.output = 'output.xlsx'
-        _output_excel(df, args.output)
-    else:
-        print("Error: 不支持的导出格式")
+    _output(df, args.output_type)
 
     print(df)
 
